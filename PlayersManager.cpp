7ef33d8cbd3f8f2a5f38f3303522a3a096fdc4e0
Ahlam Abugush <abugushahlam@campus.technion.ac.il>
2021-11-30 00:06:43 +0200

    finished structure. Nothing tested though

--- PlayersManager.cpp
+++ PlayersManager.cpp
@@ -1,2 +1,125 @@
 #include "PlayersManager.h"
 
+
+/* ------------------------------------------ Helper Functions ------------------------------------------ */
+
+static AVLNode<PlayerPointer>* arrayToTreeAux(PlayerPointer** arr, int start, int end)
+{
+	if (end < start)
+		return NULL;
+
+	int mid = (start + end) / 2;
+
+	AVLNode<PlayerPointer>* node = new AVLNode<PlayerPointer>(arr[mid]);
+	(arr[mid])->player->group_player = node;
+	
+	node->setLChild(arrayToTreeAux(arr, start, mid - 1));
+	node->setRChild(arrayToTreeAux(arr, mid + 1, end));
+
+	node->calculateStats();
+
+	return node;
+}
+
+static AVLTree<PlayerPointer>* arrayToTree(PlayerPointer** sortedLabelArray, int size)
+{
+	AVLNode<PlayerPointer>* root = arrayToTreeAux(sortedLabelArray, 0, size - 1);
+	AVLTree<PlayerPointer>* tree = new AVLTree<PlayerPointer>(root, size);
+
+	return tree;
+}
+
+static int mergeArrays(PlayerPointer** arr1, int size1, PlayerPointer** arr2, int size2, PlayerPointer** mergeArr)
+{
+	int i = 0, index1 = 0, index2 = 0;
+
+	while (index1 < size1 && index2 < size2)
+	{
+		if (*arr1[index1] < *arr2[index2]) {
+			mergeArr[i] = arr1[index1];
+			index1++;
+		}
+		else if (*arr1[index1] > *arr2[index2]) {
+			mergeArr[i] = arr2[index2];
+			index2++;
+		}
+		i++;
+	}
+
+	while (index1 < size1)
+	{
+		mergeArr[i] = arr1[index1];
+		index1++;
+		i++;
+	}
+
+	while (index2 < size2)
+	{
+		mergeArr[i] = arr2[index2];
+		index2++;
+		i++;
+	}
+
+	return i;
+}
+
+static AVLTree<PlayerPointer>* mergeLabelTrees(AVLTree<PlayerPointer>* tree1, AVLTree<PlayerPointer>* tree2)
+{
+	int size1 = tree1->getSize();
+	int size2 = tree2->getSize();
+
+	if (size1 > 0 && size2 > 0)
+	{
+		PlayerPointer** arr1 = tree1->orderedArray();
+		PlayerPointer** arr2 = tree2->orderedArray();
+		PlayerPointer** mergedArr = new PlayerPointer*[size1 + size2];
+		int mergeSize = mergeArrays(arr1, size1, arr2, size2, mergedArr);
+
+		AVLTree<PlayerPointer>* mergedTree = arrayToTree(mergedArr, mergeSize);
+
+		delete[] arr1;
+		delete[] arr2;
+		delete[] mergedArr;
+
+		return mergedTree;
+	}
+
+	else if (size1 > 0)
+	{
+		PlayerPointer** arr = tree1->orderedArray();
+		AVLTree<PlayerPointer>* tree = arrayToTree(arr, tree1->getSize());
+
+		delete[] arr;
+		return tree;
+	}
+
+	else if (size2 > 0)
+	{
+		PlayerPointer** arr = tree2->orderedArray();
+		AVLTree<PlayerPointer>* tree = arrayToTree(arr, tree2->getSize());
+
+		delete[] arr;
+		return tree;
+	}
+
+	return new AVLTree<PlayerPointer>();
+}
+
+
+/* ------------------------------------------ PlayersManager Functions ------------------------------------------ */
+
+PlayersManager::PlayersManager()
+{
+	groupTree = new AVLTree<Group>();
+	NonEmptyGroups = new AVLTree<GroupPointer>();
+	playerTree = new AVLTree<Player>();
+	playerLevels = new AVLTree<PlayerPointer>();
+}
+
+PlayersManager::~PlayersManager()
+{
+	delete groupTree;
+	delete NonEmptyGroups;
+	delete playerTree;
+	delete playerLevels;
+}
--- PlayersManager.h
+++ PlayersManager.h
@@ -6,48 +6,105 @@
 
 class Player
 {
-	int player_id;
-	int player_group_id;
+	int id;
+	int group_id;
 	int level;
 
-public:
-	AVLNode<PlayerLevel>* player_level;
-	AVLNode<PlayerLevel>* group_player;
+	friend PlayerPointer;
 
-	Player();
-	Player(int playerId, int groupId, int level);
-	~Player();
+public:
+	AVLNode<PlayerPointer>* player_level; // pointer to Player's node in playerLevels tree
+	AVLNode<PlayerPointer>* group_player; // pointer to Player's node in its group's groupPlayers tree
+
+	Player() = default;
+	Player(int playerId, int groupId, int level) :
+		id(playerId), group_id(groupId), level(level) {}
+	~Player() = default;
 
 	Player& operator=(const Player&);
-	const bool operator<(int);
-	const bool operator<(const Player&);
-	const bool operator>(int);
-	const bool operator>(const Player&);
-	const bool operator==(int);
-	const bool operator==(const Player&);
 
-	int getId();
+	const bool operator<(int id) { return this->id < id; }
+	const bool operator<(const Player& p) { return this->id < p.id; }
+	const bool operator>(int id) { return this->id > id; }
+	const bool operator>(const Player& p) { return this->id > p.id; }
+	const bool operator==(int id) { return this->id == id; }
+	const bool operator==(const Player& p) { return this->id == p.id; }
+
+	int getId() { return id; }
+	int getLevel() { return level; }
+};
+
+class PlayerPointer
+{
+public:
+	Player* player; // pointer to Player data in playerTree
+
+	PlayerPointer& operator=(const PlayerPointer& pp); 
+	
+	//const bool operator<(int pp) { return player->level < pp; }
+	//const bool operator>(int pp);
+	//const bool operator==(int pp);
+
+	const bool operator<(const PlayerPointer& pp) {
+		if (player->level == pp.player->level)
+			return player->id > pp.player->id;
+		return player->level < pp.player->level;
+	}
+	const bool operator>(const PlayerPointer& pp) {
+		if (player->level == pp.player->level)
+			return player->id < pp.player->id;
+		return player->level > pp.player->level;
+	}
+	//const bool operator==(const PlayerPointer& pp);
+
 };
 
 class Group
 {
-	int group_id;
-	int group_size;
-	PlayerLevel* highest_player;
-	AVLTree<PlayerLevel>* groupPlayers;
+	int id;
+	int size;
+	PlayerPointer* highest_player;
+	AVLTree<PlayerPointer>* groupPlayers; //sorted by level first, id second
+
+	friend GroupPointer;
+
+public:
+
+	Group() = default;
+	Group(int id) : id(id) 
+	{
+		size = 0;
+		highest_player = nullptr;
+		groupPlayers = new AVLTree<PlayerPointer>();
+	}
+
+	~Group() { delete groupPlayers; }
+
+	Group& operator=(const Group&);
+
+	const bool operator<(int id) { return this->id < id; }
+	const bool operator<(const Group& g) { return this->id < g.id; }
+	const bool operator>(int id) { return this->id > id; }
+	const bool operator>(const Group& g) { return this->id > g.id; }
+	const bool operator==(int id) { return this->id == id; }
+	const bool operator==(const Group& g) { return this->id == g.id; }
+
 };
 
-class PlayerLevel
-{
-	int player_id;
-	int level;
-	AVLNode<Player>* player;
-};
 
 class GroupPointer
 {
-	int group_id;
+public:
 	Group* group;
+
+	GroupPointer& operator=(const GroupPointer& gp);
+
+	const bool operator<(int id) { return group->id < id; }
+	const bool operator<(const GroupPointer& gp) { return group->id < gp.group->id; }
+	const bool operator>(int id) { return group->id > id; }
+	const bool operator>(const GroupPointer& gp) { return group->id > gp.group->id; }
+	const bool operator==(int id) { return group->id == id; }
+	const bool operator==(const GroupPointer& gp) { return group->id == gp.group->id; }
 };
 
 
@@ -55,8 +112,22 @@ class PlayersManager
 {
 	AVLTree<Group>* groupTree;
 	AVLTree<GroupPointer>* NonEmptyGroups;
-	AVLTree<Player>* playerTree;
-	AVLTree<PlayerLevel>* playerLevels;
+	AVLTree<Player>* playerTree; //sorted by id
+	AVLTree<PlayerPointer>* playerLevels; //sorted by level first, id second
+
+public:
+
+	PlayersManager();
+	~PlayersManager();
+
+	StatusType AddGroup(int GroupID);
+	StatusType AddPlayer(int PlayerID, int GroupID, int Level);
+	StatusType RemovePlayer(int PlayerID);
+	StatusType ReplaceGroup(int GroupID, int ReplacementID);
+	StatusType IncreaseLevel(int PlayerID, int LevelIncrease);
+	StatusType GetHighestLevel(int GroupID, int* PlayerID);
+	StatusType GetAllPlayersByLevel(int GroupID, int** Players, int* numOfPlayers);
+	StatusType GetGroupsHighestLevel(int numOfGroups, int** Players);
 };
 
 #endif // PLAYERS_MANAGER
